9cdbf3f854ee375588c3287ad298252e03742ede
Dhanushka Dilshan <dhanushka.dilshan@atlaslabs.com.au>
2022-10-27 00:39:49 +0530

    added times

--- app/src/main/java/com/example/eadassignment/RegisterActivity.java
+++ app/src/main/java/com/example/eadassignment/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.eadassignment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -9,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,8 +21,8 @@ public class RegisterActivity extends AppCompatActivity {
 
     Spinner spinner;
     TextView textView;
-    EditText stationCode,stationName;
-    Button button;
+    EditText stationCode,stationName,username, password, vehicleType;
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +32,10 @@ public class RegisterActivity extends AppCompatActivity {
         textView = findViewById(R.id.signIn);
         stationCode = findViewById(R.id.stationCode);
         stationName = findViewById(R.id.stationName);
-        button = findViewById(R.id.registerBtn);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        vehicleType = findViewById(R.id.vehicleType);
+        registerButton = findViewById(R.id.registerBtn);
 
         ///set the drop down values with visibility
         String[] value = {"User","Station Manager"};
@@ -64,12 +69,52 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
         ///intent to the sign in page using register button
-        button.setOnClickListener(new View.OnClickListener() {
+//        registerButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                checkDataEntered();
                 Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
                 startActivity(intent);
             }
         });
+    }
+
+    boolean isEmpty(EditText editText){
+        CharSequence str = editText.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
+
+    void checkDataEntered(){
+        if(isEmpty(username)){
+            Toast.makeText(this, "You must enter user name to register!", Toast.LENGTH_SHORT).show();
+        }
+        else if(isEmpty(password)){
+            Toast.makeText(this, "Enter your password", Toast.LENGTH_SHORT).show();
+        }
+        else if (isEmpty(vehicleType)){
+            Toast.makeText(this, "Password mismatch", Toast.LENGTH_SHORT).show();
+        }
+        else if (isEmpty(stationCode)){
+            Toast.makeText(this, "You must enter station code to register!", Toast.LENGTH_SHORT).show();
+        }
+        else if (isEmpty(stationName)){
+            Toast.makeText(this, "You must enter station name to register!", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            RegisterModel registerModel = new RegisterModel();
+            registerModel.setUsername(username.getText().toString());
+            registerModel.setPassword(password.getText().toString());
+            registerModel.setVehicleType(vehicleType.getText().toString());
+
+
+        }
     }
 }
--- app/src/main/java/com/example/eadassignment/StationDetailsActivity.java
+++ app/src/main/java/com/example/eadassignment/StationDetailsActivity.java
@@ -1,16 +1,29 @@
 package com.example.eadassignment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class StationDetailsActivity extends AppCompatActivity {
 
     ImageButton fuelStation, arrivalTime, departTime;
+    TextView arrivalText,departText,waitingTime;
+    String currentArrivalTime,currentDepartTime;
+    Date time1,time2;
+    long difference;
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm a");
+    int days,hours,min;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,10 +33,22 @@ public class StationDetailsActivity extends AppCompatActivity {
         fuelStation = findViewById(R.id.fuelStation);
         arrivalTime = findViewById(R.id.updateArrivalTime);
         departTime = findViewById(R.id.updateDepartTime);
+        arrivalText = findViewById(R.id.arrivalTime);
+        departText = findViewById(R.id.departTime);
+        waitingTime = findViewById(R.id.waitingTime);
 
         arrivalTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                currentArrivalTime = new SimpleDateFormat("HH:mm a", Locale.getDefault()).format(new Date());
+                Log.d("Arrival",""+currentArrivalTime);
+                arrivalText.setText(currentArrivalTime);
+
+                try {
+                    time1 = simpleDateFormat.parse(currentArrivalTime);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
                 Toast.makeText(StationDetailsActivity.this, "You Updated your Arrival Time", Toast.LENGTH_SHORT).show();
             }
         });
@@ -31,9 +56,26 @@ public class StationDetailsActivity extends AppCompatActivity {
         departTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(StationDetailsActivity.this, "You Updated your Depart Time", Toast.LENGTH_SHORT).show();
+                currentDepartTime = new SimpleDateFormat("HH:mm a", Locale.getDefault()).format(new Date());
+                Log.d("Depart",""+currentDepartTime);
+                departText.setText(currentDepartTime);
 
+                try {
+                    time2 = simpleDateFormat.parse(currentDepartTime);
+                    difference = time1.getTime() - time2.getTime();
+                    days = (int) (difference / (1000 * 60 * 60 * 24));
+                    hours = (int) ((difference - (1000*60*60*24*days)) / (1000*60*60));
+                    min = (int) (difference - (1000*60*60*24*days) - (1000*60*60*hours)) / (1000*60);
+                    hours = (hours < 0 ? -hours : hours);
+                    Log.i("======= Hours"," :: "+hours);
+                    Log.i("======= diff"," :: "+difference);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                Toast.makeText(StationDetailsActivity.this, "You Updated your Depart Time", Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 }
